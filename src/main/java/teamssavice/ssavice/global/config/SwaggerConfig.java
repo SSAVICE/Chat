b2117701a9320d959cda3393965d672e54b2c71c
hjinshin <gudwls818@gmail.com>
2026-02-27 14:47:24 +0900

    Feat: swagger 설정

---
+++ src/main/java/teamssavice/ssavice/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package teamssavice.ssavice.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import teamssavice.ssavice.global.annotation.CurrentAuth;
+
+@Configuration
+public class SwaggerConfig {
+
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(CurrentAuth.class);
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("SSAVICE CHAT API 명세서")
+                .description("SSAVICE CHAT 서비스 API 문서입니다.")
+                .version("1.0.0");
+    }
+}
