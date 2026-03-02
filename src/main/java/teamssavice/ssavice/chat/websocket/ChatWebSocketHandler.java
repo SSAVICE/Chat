0c6b07a825cc47cf23b41543e008018a2f3911b5
hjinshin <gudwls818@gmail.com>
2026-03-02 14:28:25 +0900

    Refactor: 1분 동안 PING되지 않으면 websocket 연결 끊기

--- src/main/java/teamssavice/ssavice/chat/websocket/ChatWebSocketHandler.java
+++ src/main/java/teamssavice/ssavice/chat/websocket/ChatWebSocketHandler.java
@@ -9,11 +9,15 @@ import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 import teamssavice.ssavice.chat.MessageType;
 import teamssavice.ssavice.chat.service.ChatService;
 import teamssavice.ssavice.chat.service.dto.ChatCommand;
 import teamssavice.ssavice.chat.websocket.dto.WebSocketRequest;
 import teamssavice.ssavice.room.service.RoomService;
+
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 
 @Slf4j
@@ -30,26 +34,34 @@ public class ChatWebSocketHandler implements WebSocketHandler {
         Mono<Long> subjectMono = Mono.deferContextual(ctx ->
                 Mono.just(ctx.get("subject"))
         );
+
         return subjectMono.flatMap(subject -> {
+            // 클라이언트로부터 온 pong을 output 스트림으로 보내기 위한 Sink
+            Sinks.Many<WebSocketMessage> pongSink = Sinks.many().unicast().onBackpressureBuffer();
+
             // 1. 입력 처리 (Client -> Server)
             Mono<Void> input = session.receive()
                     .map(WebSocketMessage::getPayloadAsText)
-                    .flatMap(json ->
-                            Mono.fromCallable(() -> objectMapper.readValue(json, WebSocketRequest.class))
-                                    .onErrorResume(e -> {
-                                        log.warn("잘못된 JSON 수신: {} {}", json, e.getMessage());
-                                        return Mono.empty(); // 이 메시지만 drop
-                                    })
-                    )
-                    .flatMap(req -> {
-                        if(MessageType.READ.equals(req.getMessageType())) {
-                            return chatService.readMessage(ChatCommand.Read.from(req, subject));
+                    .timeout(Duration.ofSeconds(60)) // 60초 동안 메시지가 없으면 연결 종료
+                    .flatMap(json -> {
+                        // PING을 수신하면 PONG을 Sink로 넣고 Json 파싱 건너 뜀
+                        if ("PING".equalsIgnoreCase(json)) {
+                            pongSink.tryEmitNext(session.textMessage("PONG"));
+                            return Mono.empty();
                         }
-                        ChatCommand.Chat command = ChatCommand.Chat.from(req, subject);
 
-                        return roomService.createDMRoomIfNotExist(command)
-                                .flatMap(isNewRoom -> chatService.sendMessage(command, isNewRoom));
+                        return parseJsonToRequestDto(json)
+                                .flatMap(req -> {
+                                    if (MessageType.READ.equals(req.getMessageType())) {
+                                        return chatService.readMessage(ChatCommand.Read.from(req, subject));
+                                    }
+                                    ChatCommand.Chat command = ChatCommand.Chat.from(req, subject);
+
+                                    return roomService.createDMRoomIfNotExist(command)
+                                            .flatMap(isNewRoom -> chatService.sendMessage(command, isNewRoom));
+                                });
                     })
+                    .doOnError(TimeoutException.class, e -> log.info("User {} 세션 타임아웃 - 연결 종료", subject))
                     .doFinally(signal -> log.info("Input Flux 종료: {}", signal))
                     .then();
 
@@ -65,7 +77,18 @@ public class ChatWebSocketHandler implements WebSocketHandler {
                     )
                     .doFinally(signal -> log.info("output Flux 종료: {}", signal));
 
-            return Mono.when(input, session.send(output));
+            // 채팅 메시지(output)와 PONG 응답(pongSink)를 하나로 병합
+            Flux<WebSocketMessage> combinedOutput = Flux.merge(output, pongSink.asFlux());
+            // 병합된 combinedOutput을 전송
+            return Mono.when(input, session.send(combinedOutput));
         });
+    }
+
+    private Mono<WebSocketRequest> parseJsonToRequestDto(String json) {
+        return Mono.fromCallable(() -> objectMapper.readValue(json, WebSocketRequest.class))
+                .onErrorResume(e -> {
+                    log.warn("잘못된 JSON 수신: {} {}", json, e.getMessage());
+                    return Mono.empty(); // 이 메시지만 drop
+                });
     }
 }
